d6f70fe81eb74ed9136d6e559aacb4bb655d06e6
Luis Rohten <luisaugustomelo@gmail.com>
2021-01-13 22:06:29 -0300

    creating and uncoupling the logic by controller throught repositories

--- backend-project1/src/server.ts
+++ backend-project1/src/server.ts
@@ -1,12 +1,15 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.json({ message: "Hello World!" });
-});
+// app.get("/", (request, response) => {
+//     response.json({ message: "Hello World!" });
+// });
+
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log("🚀 Server is running on port 3333");
+    console.log('🚀 Server is running on port 3333');
 });
