0ab64f2d8eed8fe629d79788e7d04d53741e97db
Luis Rohten <luisaugustomelo@gmail.com>
2021-02-02 17:11:06 -0300

    erro to start application

--- backend-project1/src/shared/infra/http/server.ts
+++ backend-project1/src/shared/infra/http/server.ts
@@ -3,31 +3,37 @@ import 'reflect-metadata';
 import express, { NextFunction, Response, Request } from 'express';
 import uploadconfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import databaseConnection from '@shared/infra/typeorm';
 import routes from './routes';
 
-import '@shared/infra/typeorm';
+async function start() {
+    const app = express();
+    app.use(express.json());
+    app.use('/files', express.static(uploadconfig.directory));
+    app.use(routes);
+    await databaseConnection();
 
-const app = express();
-app.use(express.json());
-app.use('/files', express.static(uploadconfig.directory));
-app.use(routes);
+    app.use(
+        (err: Error, request: Request, response: Response, _: NextFunction) => {
+            if (err instanceof AppError) {
+                return response.status(err.statusCode).json({
+                    status: 'error',
+                    message: err.message,
+                });
+            }
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-            status: 'error',
-            message: err.message,
-        });
-    }
+            console.log(err);
 
-    console.log(err);
+            return response.status(500).json({
+                status: 'error',
+                message: 'Internal server error',
+            });
+        },
+    );
 
-    return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
+    app.listen(3333, () => {
+        console.log('🚀 Server is running on port 3333');
     });
-});
+}
 
-app.listen(3333, () => {
-    console.log('🚀 Server is running on port 3333');
-});
+start();
--- backend-project1/src/shared/infra/typeorm/index.ts
+++ backend-project1/src/shared/infra/typeorm/index.ts
@@ -1,3 +1,8 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+async function databaseConnection() {
+    return createConnection();
+}
+
+export default databaseConnection;
