6ebfaf728e7e3da27bd45247ea062513127d549d
Luis Rohten <luisaugustomelo@gmail.com>
2021-02-17 00:57:41 -0300

    making first test and config coverage

---
+++ backend-project1/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmedntDTO from '@modules/appointments/dto/ICreateAppointmentDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+    private appointments: Appointment[] = [];
+
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+        const findAppointment = this.appointments.find(
+            appointment => appointment.date === date,
+        );
+
+        return findAppointment;
+    }
+
+    public async create({
+        date,
+        provider_id,
+    }: ICreateAppointmedntDTO): Promise<Appointment> {
+        const appointment = new Appointment();
+
+        Object.assign(appointment, { id: uuid(), date, provider_id });
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+}
+
+export default AppointmentsRepository;
