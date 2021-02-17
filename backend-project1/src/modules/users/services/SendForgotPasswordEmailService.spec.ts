6bec16a740c679228e91997ecab0ea5a3af18b12
Luis Rohten <luisaugustomelo@gmail.com>
2021-02-17 19:14:52 -0300

    added method using tdd to send email

---
+++ backend-project1/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,46 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('sendForgotPasswordEmail', () => {
+    it('should be able to recover the password using the email', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeMailProvider = new FakeMailProvider();
+        const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+            fakeUsersRepository,
+            fakeMailProvider,
+        );
+
+        const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+        await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        await sendForgotPasswordEmail.execute({
+            email: 'johndoe@example.com',
+        });
+
+        expect(sendMail).toHaveBeenCalled();
+    });
+
+    it('should not be able to recover a non-existing user password', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeMailProvider = new FakeMailProvider();
+        const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+            fakeUsersRepository,
+            fakeMailProvider,
+        );
+
+        await expect(
+            sendForgotPasswordEmail.execute({
+                email: 'johndoe@example.com',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
