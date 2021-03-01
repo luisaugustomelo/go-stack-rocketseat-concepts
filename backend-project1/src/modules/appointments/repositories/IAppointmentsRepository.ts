c9eb81dde302313c7806c5289310e7264f7fba4b
Luis Rohten <luisaugustomelo@gmail.com>
2021-02-28 22:31:51 -0300

    listing availability days

--- backend-project1/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend-project1/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '@modules/appointments/dto/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dto/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
     create(data: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
+    findAllMonthFromProvider(
+        data: IFindAllInMonthFromProviderDTO,
+    ): Promise<Appointment[]>;
 }
--- backend-project1/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend-project1/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,8 +1,9 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmedntDTO from '@modules/appointments/dto/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dto/IFindAllInMonthFromProviderDTO';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
@@ -15,6 +16,21 @@ class AppointmentsRepository implements IAppointmentsRepository {
         );
 
         return findAppointment;
+    }
+
+    public async findAllMonthFromProvider({
+        provider_id,
+        month,
+        year,
+    }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+        const appointments = this.appointments.filter(
+            appointment =>
+                appointment.provider_id === provider_id &&
+                getMonth(appointment.date) + 1 === month &&
+                getYear(appointment.date) === year,
+        );
+
+        return appointments;
     }
 
     public async create({
