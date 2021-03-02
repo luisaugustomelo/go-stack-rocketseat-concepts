7180dca5560da162efa969c98a7ac64b997a7436
Luis Rohten <luisaugustomelo@gmail.com>
2021-03-01 22:06:40 -0300

    sending notifications and refactoring tests

--- backend-project1/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend-project1/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,10 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 
 interface IRequest {
     provider_id: string;
@@ -15,6 +16,9 @@ class CreateAppointmentService {
     constructor(
         @inject('AppointmentsRepository')
         private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('NotifcationsRepository')
+        private notificationsRepository: INotificationsRepository,
     ) {}
 
     public async execute({
@@ -56,6 +60,11 @@ class CreateAppointmentService {
             user_id,
         });
 
+        const formatDate = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+        await this.notificationsRepository.create({
+            recipient_id: provider_id,
+            content: `Novo agendamento para dia ${formatDate} `,
+        });
         return appointment;
     }
 }
