cd642875714636ae5fbc42d17a8285535c6f7e64
Luis Rohten <luisaugustomelo@gmail.com>
2021-03-03 00:46:52 -0300

    added redis to do requests faster than before

--- backend-project1/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend-project1/src/modules/appointments/services/CreateAppointmentService.ts
@@ -5,6 +5,7 @@ import AppError from '@shared/errors/AppError';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
     provider_id: string;
@@ -19,6 +20,9 @@ class CreateAppointmentService {
 
         @inject('NotifcationsRepository')
         private notificationsRepository: INotificationsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({
@@ -65,6 +69,14 @@ class CreateAppointmentService {
             recipient_id: provider_id,
             content: `Novo agendamento para dia ${formatDate} `,
         });
+
+        await this.cacheProvider.invalidate(
+            `provider-appointments:${provider_id}:${format(
+                appointmentDate,
+                'yyyy-M-d',
+            )}`,
+        );
+
         return appointment;
     }
 }
--- backend-project1/src/modules/users/services/CreateUserService.ts
+++ backend-project1/src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
@@ -18,6 +19,8 @@ class CreateUserService {
         private usersRepository: IUsersRepository,
         @inject('HashProvider')
         private hashProvider: IHashProvider,
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -33,6 +36,8 @@ class CreateUserService {
             email,
             password: hashedPassword,
         });
+
+        await this.cacheProvider.invalidatePrefix('providers-list');
 
         return user;
     }
